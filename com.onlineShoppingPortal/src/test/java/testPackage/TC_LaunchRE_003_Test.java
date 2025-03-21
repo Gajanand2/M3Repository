80610b34fe1ec6fe11e28325965a70824a6115af
Administrator <Administrator@DESKTOP-FLBM77S>
2025-03-21 12:17:24 +0530

    Second Commit Version 0.2

---
+++ com.onlineShoppingPortal/src/test/java/testPackage/TC_LaunchRE_003_Test.java
@@ -0,0 +1,18 @@
+package testPackage;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC_LaunchRE_003_Test {
+  @Test
+  public void launchRE() {
+	  WebDriver driver=new ChromeDriver();
+		 driver.manage().window().maximize();
+		 driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+		 driver.get("https://www.royalenfield.com/");
+		  
+  }
+}
